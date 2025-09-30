4338751b6c158753e2a3834aa53bff31c9238e7b
mahmudjanov4646 <mahmudjanov46@gmail.com>
2025-09-30 17:41:05 +0500

    My Ticket

--- src/configs/database.js
+++ src/configs/database.js
@@ -13,7 +13,6 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
   }
